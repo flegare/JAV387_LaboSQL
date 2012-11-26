d2d69b290472a5e011f2953d249a772b5d0439c2
Francois Legare <francois.legare@gmail.com>
2012-11-26 18:51:08 -0500

    initial project commit

---
+++ src/com/example/laboSqlite/LabSqlLiteActivity.java
@@ -0,0 +1,13 @@
+package com.example.laboSqlite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LabSqlLiteActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
